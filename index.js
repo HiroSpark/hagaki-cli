c96659911ff78e7e91da5c82d6fc330244a35eb1
HiroSpark <73699844+HiroSpark@users.noreply.github.com>
2022-01-05 19:03:53 +0900

    feat: add recipients selection feature by regex

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const cmdArgs = require("command-line-args");
 
 const optsDef = [
   {
+    name: "filter",
+    defaultOption: true
+  },
+  {
     name: "file",
     alias: "f",
     defaultValue: "data.json"
@@ -33,11 +37,13 @@ if (!fs.existsSync(outputDir)) {
 }
 
 const senderMembers = getAllMembers(sender.name, sender.family);
+const filterRegex = new RegExp(opts.filter);
 
 Object.keys(recipient).forEach((recipienetName) => {
-  const recipientData = recipient[recipienetName];
-  const recipientMembers = getAllMembers(recipienetName, recipientData.family);
-  const texFileBody = 
+  if (filterRegex.test(recipienetName)) {
+    const recipientData = recipient[recipienetName];
+    const recipientMembers = getAllMembers(recipienetName, recipientData.family);
+    const texFileBody = 
 `\\documentclass[nenga]{hagaki}
 \\sender{
   postal_code = ${sender.postalCode},
@@ -51,24 +57,25 @@ Object.keys(recipient).forEach((recipienetName) => {
   address     = ${recipientData.address}
 }
 \\end{document}`;
-  const fileName = recipienetName.replace(" ", "_");
-  const texFilePath = path.format({ dir: outputDir, name: fileName, ext: ".tex" });
-  fs.promises.writeFile(texFilePath, texFileBody)
-    .then(() => {
-      console.log("  >> Generated file: " + fileName + ".tex");
-      const command = "lualatex " + fileName + ".tex";
-      const opts = {
-        cwd: outputDir
-      }
-      cp.exec(command, opts, (error) => {
-        if (error) {
-          throw error;
-        } else {
-          console.log("  >> Generated file: " + fileName + ".pdf");
+    const fileName = recipienetName.replace(" ", "_");
+    const texFilePath = path.format({ dir: outputDir, name: fileName, ext: ".tex" });
+    fs.promises.writeFile(texFilePath, texFileBody)
+      .then(() => {
+        console.log("  >> Generated file: " + fileName + ".tex");
+        const command = "lualatex " + fileName + ".tex";
+        const opts = {
+          cwd: outputDir
         }
+        cp.exec(command, opts, (error) => {
+          if (error) {
+            throw error;
+          } else {
+            console.log("  >> Generated file: " + fileName + ".pdf");
+          }
+        });
+      })
+      .catch((error) => {
+        throw error;
       });
-    })
-    .catch((error) => {
-      throw error;
-    });
+  }
 });
